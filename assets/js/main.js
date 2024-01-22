18826051f51ef8d8d9fe87a70aa77bd9ccb7defd
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-22 18:33:56 +0100

    feat: start btn working. fix interval overlaps. fix: iteration numbers now for array length

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,43 +3,31 @@ const images = [
         url: './assets/img/miles-morales.jpg',
         titolo: 'Marvel’s Spider-Man: Miles Morales',
         descrizione: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-
-        index : 0,
     },
     {
         url: './assets/img/hollow-knight.jpg',
         titolo: 'Hollow Knight',
         descrizione: 'Descend into the Dark',
-
-        index : 1,
     },
     {
         url: './assets/img/GOF-Ragnarok.webp',
         titolo: 'God of War Ragnarök',
         descrizione: 'Embark on an epic and heartfelt journey as Kratos and Atreus struggle with holding on and letting go',
-
-        index : 2,
     },
     {
         url: './assets/img/bugia-non-esiste.jpg',
         titolo: 'Silksong',
         descrizione: 'Explore, fight and survive as you ascend to the peak of a land ruled by silk and song.',
-
-        index : 3,
     },
     {
         url: './assets/img/d2-finalshape.jpg',
         titolo: 'Destiny 2 - Final Shape',
-        descrizione: 'Embark on a perilous journey into the heart of the Traveler, rally the Vanguard, and end the War of Light and Darkness.',
-
-        index : 4,
+        descrizione: 'Embark on a perilous journey into the heart of the Traveler, rally the Vanguard, and end the War of Light and Darkness.',        
     },
     {
         url: './assets/img/remnant2.jpg.avif',
         titolo: 'Remnant 2',
         descrizione: 'Remnant II pits survivors of humanity against new deadly creatures and god-like bosses across terrifying worlds.',
-
-        index : 5,
     },
 ]
 
@@ -63,79 +51,87 @@ titleHtml.innerHTML = `${images[active].titolo}`;
 descriptionHtml.innerHTML = `${images[active].descrizione}`;
 
 //creazione delle thumbnail con evento al click per cambiare l'immagine principale
-images.forEach(element => {
+images.forEach((element, index) => {
 
     const thumbnailImg = document.createElement('img');
     thumbnailImg.setAttribute('src', `${element.url}`);
     thumbnailImg.classList.add('smallone');
 
     thumbnailImg.addEventListener('click', function(){        
-        active = element.index;
+        active = index;
 
         activePhoto.innerHTML = `<img class="bigone h-100 " src="${element.url}" alt="">`;
 
         titleHtml.innerText = element.titolo;
         descriptionHtml.innerText = element.descrizione;
     })
-
     thumbnail.append(thumbnailImg);    
 });
 
 //bottone per scorrere le immagini verso il basso
 btnDown.addEventListener('click', function(){
-    active++;
-    if(active > 5){
-        active = 0;
-    }
-    sliderImage();
+    nextImage()
 })
 
 //bottone per scorrere le immagini verso l'alto
 btnUp.addEventListener('click', function(){
-    active--;
-    if(active < 0){
-        active = 5;
-    }
+    previousImage()
     sliderImage();
 })
 //funzioni temporali
 let autoplayFunction;
 let reverseFunction;
+let playFlag;
+let initialFlag = false;
 
 //autoplay function
 autoplay.addEventListener('click', function(){
-    clearInterval(reverseFunction);
+    clearBothInterval();
+    initialFlag = true;
 
     autoplayFunction = setInterval(() => {
-        active++
-        if(active > 5){
-            active = 0;
-        }
+        nextImage()
         sliderImage();
-    }, 3000);
-
+    }, 1000);
+    return playFlag = true;
+    
 })
 
 //reverse function
 reverse.addEventListener('click', function(){
-    clearInterval(autoplayFunction);
+    clearBothInterval();
+    initialFlag = true;
 
     reverseFunction = setInterval(() => {
-        active--
-        if(active < 0){
-            active = 5;
-        }
+        previousImage()
         sliderImage();
-        
-    }, 3000);
-    reverse.setAttribute('')
+
+    }, 1000);
+    return playFlag = false;
+    
 })
 
 
 stopBtn.addEventListener('click', function(){
-    clearInterval(autoplayFunction);
-    clearInterval(reverseFunction);
-    flag = false;
+    clearBothInterval()
+})
+
+start.addEventListener('click', function(){
+    if(initialFlag){
+        if(playFlag == true){
+            clearBothInterval();
+    
+            autoplayFunction = setInterval(() => {
+                nextImage()
+            }, 1000);
+        }else{
+            clearBothInterval()
+    
+            reverseFunction = setInterval(() => {
+                previousImage()
+            }, 1000);
+        }
+    }
 })
 
 //funziona che genera nel DOM le immagini
@@ -143,5 +139,27 @@ function sliderImage(){
     activePhoto.innerHTML = `<img class="bigone h-100 " src="${images[active].url}" alt="">`;
     titleHtml.innerText = images[active].titolo;
     descriptionHtml.innerText = images[active].descrizione;
+}
+//reset di entrambe le funzioni temporali
+function clearBothInterval(){
+    clearInterval(autoplayFunction);
+    clearInterval(reverseFunction);
+}
+//immagine successiva
+function nextImage(){
+    active++;
+    if(active > images.length-1){
+        active = 0;
+    }
+    sliderImage();
+}
+
+//immagine precedente
+function previousImage(){
+    active--;
+    if(active < 0){
+        active = images.length-1;
+    }
+    sliderImage();
 }
 
